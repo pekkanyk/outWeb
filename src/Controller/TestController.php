66634ebf5bbe42d8f1f2b6ac549bd4564a7e609f
Pekka Nykänen <>
2021-04-10 22:19:22 +0300

    Testing out

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Controller;
+
+/**
+ * Description of OutletProductController
+ *
+ * @author qru19
+ */
+class TestController {
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return new Response(
+                '<html><body>Random numero: '.$number.'</body></html>'
+        );
+    }
+}
