8eccf3783f0af7dbba1f32bdcdfd43b65c5973dc
pekkanyk <pekka.nykanen@outlook.com>
2021-04-11 19:53:04 +0300

    twig testing + fix

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TestController {
+class TestController extends AbstractController{
     /**
      *@Route("/test/number")
      */
