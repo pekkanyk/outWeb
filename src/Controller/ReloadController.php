f80ea3699eea19c648d937051958cd43cbd0f67a
pekkanyk <pekka.nykanen@outlook.com>
2021-04-14 21:53:26 +0300

    first json queries

--- src/Controller/ReloadController.php
+++ src/Controller/ReloadController.php
@@ -11,15 +11,31 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ReloadController extends AbstractController{
+    private $client;
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+    
+    public function getJsonFromVk(string $i):string
+    {
+        $response = $this->client->request(
+                'GET',
+                'https://web-api.service.verkkokauppa.com/search?context=customer_returns_page&pageNo='.$i
+        );
+        return $response->getContent();
+    }
     /**
      *@Route("/reload")
      */
     public function reloadJson(): Response
     {
+        $string = $this->getJsonFromVk('0');
         
-        
-        return $this->redirectToRoute('homepage');
+        //return $this->redirectToRoute('homepage');
+        return new Response($string);
     }
 }
